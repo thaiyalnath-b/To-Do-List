81e53fa22bbd549705852c3d8d3a53dd5844b056
ThaiyalnathB <thaiyalnath2005@gmail.com>
2025-09-25 09:50:34 +0530

    File Edited

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
+const input = document.getElementById('taskInput');
+const status = document.getElementById('taskStatus');
 const adding = document.getElementById('add');
-const deleting = document.getElementById('dlt');
+const deleting=document.getElementById('dlt');
 const lists = document.getElementById('taskList');
 
-let taskList = [];
+let taskList =[];
 function render(){
-    lists.innerHTML= '';
+    lists.innerHTML = '';
     taskList.forEach((task, index)=>{
         const listItem = document.createElement('li');
-        listItem.innerHTML = `${task.taskname} - ${task.status} <button class='dltt' onClick='deleteTask(${index})'>Delete</button>`;
+        listItem.innerHTML = `${task.taskname} - ${task.taskstatus} <button class='delet' onClick='deleteTask(${index})'>Delete</button>`;
         lists.appendChild(listItem);
     });
 }
-adding.addEventListener('click', ()=>{
-    const taskInput= document.getElementById('taskInput');
-    const taskStatus = document.getElementById('taskStatus');
-    taskList.unshift({taskname:taskInput.value, status:taskStatus.value});
+
+adding.addEventListener('click',()=>{
+    taskList.unshift({taskname:taskInput.value, taskstatus:taskStatus.value});
     render();
+
     taskInput.value='';
-    taskStatus.value= 'Completed';
+    taskStatus.value='Completed';
 });
 
 deleting.addEventListener('click', ()=>{
-   taskList=[];
-   render(); 
+    taskList = [];
+    render();
 });
+
 window.deleteTask = (index)=>{
     taskList.splice(index, 1);
     render();
